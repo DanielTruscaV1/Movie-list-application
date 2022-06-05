af4e49e0f7aaae055a35b0205e68067551c7ee42
DanielTruscaV1 <danieltrusca2016@gmail.com>
2022-06-05 17:16:52 +0300

    Finished the first version of the application.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
 
 const MovieList = (props) => {
-    return (
-        <>
-            {props.movies.map((movie, index) => <div><img src={movie.Poster}/></div> )}
-        </>
-    );
-}
+    const FavouriteComponent = props.favouriteComponent;
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+                    <div onClick={() => {props.handleFavouritesClick(movie)}} className="overlay d-flex align-items-center justify-content-center">
+						<FavouriteComponent/>
+					</div>
+				</div>
+			))}
+		</>
+	);
+};
 
 export default MovieList;
